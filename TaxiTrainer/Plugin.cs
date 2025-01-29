730a23fac61b10a9b152260ee1039393261c5989
alwaysintreble <mmmcheese158@gmail.com>
2025-01-28 23:19:15 -0600

    initial TaxiTrainer project setup

---
+++ TaxiTrainer/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using Chauffeur.Managers;
+using Chauffeur.Utils;
+using TaxiTrainer.Trainer;
+
+namespace TaxiTrainer;
+
+[BepInDependency("com.alwaysintreble.Chauffeur")]
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string PluginGuid = "com.alwaysintreble.TaxiTrainer";
+    public const string PluginName = "TaxiTrainer";
+    public const string PluginVersion = "0.1.0";
+
+    public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
+    public static ManualLogSource ChauffeurLogger;
+    public static Plugin Instance;
+
+    private void Awake()
+    {
+        ChauffeurLogger = Logger;
+        Instance = this;
+
+        MenuManager.AddButtons += () =>
+        {
+            var trainerMenu = new TrainerMenu();
+            MenuManager.AddPauseMenuButton(new MenuButton(() => "Trainer", trainerMenu.LoadMenu, () => true));
+        };
+    }
+}
---
+++ TaxiTrainer/Trainer/TrainerMenu.cs
@@ -0,0 +1,47 @@
+﻿using Chauffeur.Utils;
+using TaxiTrainer.Managers;
+
+namespace TaxiTrainer.Trainer;
+
+public class TrainerMenu : CustomMenu
+{
+    private bool _allLevelsUnlocked;
+
+    public TrainerMenu() : base("Trainer")
+    {
+        _allLevelsUnlocked = true;
+        foreach (var data in Data.levelDataList)
+        {
+            if (data.levelUnlocked) continue;
+            _allLevelsUnlocked = false;
+            break;
+        }
+        // AddButton(new MenuButton(() => _allLevelsUnlocked ? "Lock All Levels" : "Unlock All Levels", ToggleLockedLevels));
+        AddButton(new MenuButton(() => Master.instance.DEBUG ? "Disable Debug" : "Enable Debug",
+            () => Master.instance.DEBUG = !Master.instance.DEBUG));
+        AddButton(new MenuButton(
+            () => Master.instance.SHOW_TESTER_BUTTONS ? "Disable Inputs Overlay" : "Enable Inputs Overlay",
+            () => Master.instance.SHOW_TESTER_BUTTONS = !Master.instance.SHOW_TESTER_BUTTONS));
+
+        var controlsMenu = new PlayerControlsMenu();
+        AddButton(new MenuButton("Controls Restrictions", controlsMenu.LoadMenu));
+        
+        AddLevelManagementButtons();
+    }
+
+    private void AddLevelManagementButtons()
+    {
+        var levelManager = new LevelManager();
+        AddButton(new MenuButton(levelManager.GetDeathTrainerText, levelManager.ChangeDeathBehavior));
+        AddButton(new MenuButton(LocalizationHelper.GoBackText, GoBack));
+    }
+
+    private void ToggleLockedLevels()
+    {
+        _allLevelsUnlocked = !_allLevelsUnlocked;
+        foreach (var data in Data.levelDataList)
+        {
+            data.levelUnlocked = _allLevelsUnlocked;
+        }
+    }
+}
